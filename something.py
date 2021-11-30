41c1fcdcceb222d29d51ef6349ea43d0dac23b31
anddreyka <50086480+anddreyka@users.noreply.github.com>
2021-11-30 11:38:43 +0300

    Create something.py

---
+++ something.py
@@ -0,0 +1,15 @@
+def kek(n):
+    arr = list(True for i in range(n))
+    res = []
+    arr[0] = arr[1] = False
+    p = 2
+    while p**2 < n:
+        if arr[p]:
+            for i in range(p**2, n):
+                if i % p == 0 and arr[i]:
+                    arr[i] = False
+        p += 1
+    for i in range(len(arr)):
+        if arr[i]:
+            res.append(i)
+    return res
